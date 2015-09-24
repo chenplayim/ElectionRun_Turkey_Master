e534800fa1f2f130f6b350f7f3d5388c5e9c5049
chenplayim <chen@play.im>
2015-09-24 12:46:31 +0300

    4

--- ElectionRun_Turkey/Assets/MyHeyzap.cs
+++ ElectionRun_Turkey/Assets/MyHeyzap.cs
@@ -9,5 +9,15 @@ public class MyHeyzap : MonoBehaviour {
 	void Awake()
 	{
 		HeyzapAds.start("4f2ead6c706a5286add45879429b0be5", HeyzapAds.FLAG_NO_OPTIONS);
+
 	}
+
+	public void TestMediation()
+	{
+		HeyzapAds.showMediationTestSuite ();
+
+	}
+
+
+
 }
